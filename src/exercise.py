3b7276467d84ed2dedc5964bc09adabc1e1bf0c0
ryagif <21254@bridgend.ac.uk>
2020-09-25 11:29:36 +0000

    hello

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,7 @@
 def main():
     #write your code below this line
-
+  first = input ('Bob')
+  second = input('a bulider')
+  thrid = input('Once upon a time there was Bob, who was a builder. On the way to work, Bob reflected on life. Perhaps Bob will not be a builder forever.')
 if __name__ == '__main__':
     main()
